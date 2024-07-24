4af48e3609c48aed75326f75bb7bc93c7a5cb01b
ChristopherS24 <chryskies@gmail.com>
2024-07-24 23:53:16 +0200

    inserita la logica del gioco

--- js/script.js
+++ js/script.js
@@ -1,19 +1,46 @@
-document.getElementById('gridButton').addEventListener('click', generateGrid);
+const gridButton = document.getElementById(`gridButton`);
 
-function generateGrid() {
-    const gridContainer = document.getElementById('gridContainer');
+gridButton.addEventListener(`click`,function (event) {
+    //blocca comportamento di default
+    event.preventDefault();
 
-    gridContainer.innerHTML = ''
+    const gridContainer = document.getElementById(`gridContainer`);
+    
+    let cellsNumber = 100;
+    
+    const bombs = [];
+    while (bombs.length < 16) {
+        const randomNumber = generateRandomNumber(1, cellsNumber);
+        // console.log(`random number:`, randomNumber, typeof randomNumber);
 
-for (let i = 1; i <= 100; i++) {
-    const cell = document.createElement('div');
-    cell.className = 'gridCell';
-    cell.innerText = i;
-    cell.addEventListener('click', function() {
-        cell.classList.toggle('clean');
-        console.log('Cella cliccata:', i);
-    });
-    gridContainer.append(cell);
+    if (bombs.includes(randomNumber) == false) {
+            bombs.push(randomNumber);
+        }
     }
+    console.log(`bombs`, bombs, typeof bombs);
+
+    for (let i = 1; i <= cellsNumber; i++) {
+        const newCell = document.createElement(`div`);
+        newCell.innerText = i;
+        newCell.addEventListener(`click`, function () {        
+            newCell.classList.toggle(`lightcoral`);
+            console.log(newCell.innerHTML);
+            
+            const cellNumber = parseInt(this.innerText);
+            console.log (cellNumber);
+
+            if (bombs.includes(cellNumber)){
+                this.classList.add('bomb');
+            }
+            else {this.classList.add('notbomb');
+            }
+        });
+        gridContainer.append(newCell);
+    }
+})
+
+
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
